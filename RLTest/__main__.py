77d53764b6bdd6c3aba469348b7f8f9266af17c7
Mark Nunberg <mnunberg@haskalah.org>
2018-09-14 10:17:26 -0400

    Move downloading enterprise binaries to own file

--- RLTest/__main__.py
+++ RLTest/__main__.py
@@ -3,8 +3,6 @@ import os
 import cmd
 import traceback
 import sys
-import subprocess
-import platform
 import shutil
 import inspect
 import unittest
@@ -13,13 +11,8 @@ import shlex
 from env import Env
 from utils import Colors
 from RLTest.loader import TestLoader
+from RLTest.Enterprise import binaryrepo
 
-RLTest_WORKING_DIR = os.path.expanduser('~/.RLTest/')
-RLTest_ENTERPRISE_VERSION = '5.2.0'
-RLTest_ENTERPRISE_SUB_VERSION = '14'
-OS_NAME = platform.dist()[2]
-RLTest_ENTERPRISE_TAR_FILE_NAME = 'redislabs-%s-%s-%s-amd64.tar' % (RLTest_ENTERPRISE_VERSION, RLTest_ENTERPRISE_SUB_VERSION, OS_NAME)
-RLTest_ENTERPRISE_URL = 'https://s3.amazonaws.com/rlec-downloads/%s/%s' % (RLTest_ENTERPRISE_VERSION, RLTest_ENTERPRISE_TAR_FILE_NAME)
 RLTest_CONFIG_FILE_PREFIX = '@'
 RLTest_CONFIG_FILE_NAME = 'config.txt'
 
@@ -91,7 +84,7 @@ parser = CustomArgumentParser(fromfile_prefix_chars=RLTest_CONFIG_FILE_PREFIX,
                               formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                               description='Test Framework for redis and redis module')
 
-
+parser.add_subparsers()
 parser.add_argument(
     '--module', default=None,
     help='path to the module file')
@@ -109,7 +102,7 @@ parser.add_argument(
     help='path to the oss redis binary')
 
 parser.add_argument(
-    '--enterprise-redis-path', default=os.path.join(RLTest_WORKING_DIR, 'opt/redislabs/bin/redis-server'),
+    '--enterprise-redis-path', default=os.path.join(binaryrepo.REPO_ROOT, 'opt/redislabs/bin/redis-server'),
     help='path to the entrprise redis binary')
 
 parser.add_argument(
@@ -152,11 +145,11 @@ parser.add_argument(
     help='run env with slaves enabled')
 
 parser.add_argument(
-    '--proxy-binary-path', default=os.path.join(RLTest_WORKING_DIR, 'opt/redislabs/bin/dmcproxy'),
+    '--proxy-binary-path', default=os.path.join(binaryrepo.REPO_ROOT, 'opt/redislabs/bin/dmcproxy'),
     help='dmc proxy binary path')
 
 parser.add_argument(
-    '--enterprise-lib-path', default=os.path.join(RLTest_WORKING_DIR, 'opt/redislabs/lib/'),
+    '--enterprise-lib-path', default=os.path.join(binaryrepo.REPO_ROOT, 'opt/redislabs/lib/'),
     help='path of needed libraries to run enterprise binaries')
 
 parser.add_argument(
@@ -236,7 +229,8 @@ class RLTest:
             self.args.stop_on_failure = True
 
         if self.args.download_enterprise_binaries:
-            self._downloadEnterpriseBinaries()
+            br = binaryrepo.BinaryRepository()
+            br.download_binaries()
 
         if self.args.clear_logs:
             try:
@@ -279,40 +273,6 @@ class RLTest:
 
     def _convertArgsType(self):
         pass
-
-    def _downloadEnterpriseBinaries(self):
-        binariesName = 'binaries.tar'
-        print Colors.Yellow('installing enterprise binaries')
-        print Colors.Yellow('creating RLTest working dir: %s' % RLTest_WORKING_DIR)
-        try:
-            shutil.rmtree(RLTest_WORKING_DIR)
-            os.makedirs(RLTest_WORKING_DIR)
-        except Exception:
-            pass
-
-        print Colors.Yellow('download binaries')
-        args = ['wget', RLTest_ENTERPRISE_URL, '-O', os.path.join(RLTest_WORKING_DIR, binariesName)]
-        self.process = subprocess.Popen(args=args, stdout=sys.stdout, stderr=sys.stdout)
-        self.process.wait()
-        if self.process.poll() != 0:
-            raise Exception('failed to download enterprise binaries from s3')
-
-        print Colors.Yellow('extracting binaries')
-        debFileName = 'redislabs_%s-%s~%s_amd64.deb' % (RLTest_ENTERPRISE_VERSION, RLTest_ENTERPRISE_SUB_VERSION, OS_NAME)
-        args = ['tar', '-xvf', os.path.join(RLTest_WORKING_DIR, binariesName), '--directory', RLTest_WORKING_DIR, debFileName]
-        self.process = subprocess.Popen(args=args, stdout=sys.stdout, stderr=sys.stdout)
-        self.process.wait()
-        if self.process.poll() != 0:
-            raise Exception('failed to extract binaries to %s' % self.RLTest_WORKING_DIR)
-
-        # TODO: Support centos that does not have dpkg command
-        args = ['dpkg', '-x', os.path.join(RLTest_WORKING_DIR, debFileName), RLTest_WORKING_DIR]
-        self.process = subprocess.Popen(args=args, stdout=sys.stdout, stderr=sys.stdout)
-        self.process.wait()
-        if self.process.poll() != 0:
-            raise Exception('failed to extract binaries to %s' % self.RLTest_WORKING_DIR)
-
-        print Colors.Yellow('finished installing enterprise binaries')
 
     def takeEnvDown(self, fullShutDown=False):
         if not self.currEnv:
