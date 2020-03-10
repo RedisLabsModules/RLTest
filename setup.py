03445c9527c82cd5fec3228345c4a87c573910f7
filipecosta90 <filipecosta.90@gmail.com>
2020-03-10 17:16:12 +0000

    [add] limiting the redis-py and redis-py-cluster versions

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     description="Redis Labs Test Framework, allow to run tests on redis and modules on verity of environments.",
     packages=find_packages(),
     install_requires=[
-        'redis>=3.0.0,<3.5.0',
-        'redis-py-cluster>=2.0.0',
+        'redis>=3.0.0,<3.1.0',
+        'redis-py-cluster==2.0.0',
         'psutil'
     ],
     entry_points='''
