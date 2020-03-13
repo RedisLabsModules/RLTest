1859804669cd65d2d9ccf22c854038607445e914
filipecosta90 <filipecosta.90@gmail.com>
2020-03-13 22:58:15 +0000

    [add] added dmc and enterprise test files

--- tests/unit/test_redis_enterprise_cluster.py
+++ tests/unit/test_redis_enterprise_cluster.py
@@ -1,21 +1,30 @@
 from unittest import TestCase
 
 
-class TestShard(TestCase):
+class TestEnterpriseRedisClusterEnv(TestCase):
+    def test_wait_cluster(self):
+        pass
+
     def test_dump_and_reload(self):
         pass
 
-    def test__get_node_address(self):
-        pass
-
-    def test_send_request(self):
-        pass
-
-    def test_get_master_port(self):
+    def test_get_cluster_connection(self):
         pass
 
     def test_get_connection(self):
         pass
 
-    def test_broadcast(self):
+    def test_get_master_nodes_list(self):
+        pass
+
+    def test_get_ossmaster_nodes_connection_list(self):
+        pass
+
+    def test_is_up(self):
+        pass
+
+    def test_is_unix_socket(self):
+        pass
+
+    def test_is_tcp(self):
         pass
