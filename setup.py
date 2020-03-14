795567c495dcda17436f4fd2bb9c1a171b980f80
filipecosta90 <filipecosta.90@gmail.com>
2020-03-14 03:29:29 +0000

    [add] RLTest working with redis>=3.0.0

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
+
 setup(
     name='RLTest',
     version='0.2.1',
     description="Redis Labs Test Framework, allow to run tests on redis and modules on verity of environments.",
     packages=find_packages(),
     install_requires=[
-        'redis>=3.0.0,<3.1.0',
-        'redis-py-cluster==2.0.0',
+        'redis>=3.0.0',
+        'redis-py-cluster@git+ssh://git@github.com/Grokzen/redis-py-cluster.git@master#egg=redis-py-cluster',
         'psutil'
     ],
     entry_points='''
