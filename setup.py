fa1bb8d81d928ae228e8e775a9330e7557ead174
filipecosta90 <filipecosta.90@gmail.com>
2020-12-16 15:28:45 +0000

    Preparing for release automation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='RLTest',
     version='0.2.1',
-    description="Redis Labs Test Framework, allow to run tests on redis and modules on verity of environments.",
+    description="Redis Labs Test Framework, allow to run tests on redis and modules on a variety of environments.",
     packages=find_packages(),
     install_requires=[
         'redis>=3.0.0',
