431f9c13676569e661987ae57aa7bf7a9fee0b62
Rafi Einstein <rafi@redislabs.com>
2021-01-24 15:37:12 +0200

    Exported Defaults class

--- RLTest/__init__.py
+++ RLTest/__init__.py
@@ -1,8 +1,9 @@
-from RLTest.env import Env
+from RLTest.env import Env, Defaults
 from RLTest.redis_std import StandardEnv
 from ._version import __version__
 
 __all__ = [
+    'Defaults',
     'Env',
     'StandardEnv'
 ]
