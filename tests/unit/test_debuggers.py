acdb3982effa0e1ea7555218ccad36f3d2d31d85
DvirDukhan <dvir@redis.com>
2021-12-02 09:52:12 +0200

    fixed tests

--- tests/unit/test_debuggers.py
+++ tests/unit/test_debuggers.py
@@ -7,17 +7,17 @@ class TestValgrind(TestCase):
     def test_generate_command_default(self):
         default_valgrind = Valgrind(options="")
         cmd_args = default_valgrind.generate_command()
-        assert ['valgrind', '--error-exitcode=1', '--leak-check=full', '--errors-for-leak-kinds=definite',
+        assert ['valgrind', '--error-exitcode=255', '--leak-check=full', '--errors-for-leak-kinds=definite',
                 ] == cmd_args
 
     def test_generate_command_supression(self):
         default_valgrind = Valgrind(options="", suppressions="file")
         cmd_args = default_valgrind.generate_command()
-        assert ['valgrind', '--error-exitcode=1', '--leak-check=full', '--errors-for-leak-kinds=definite',
+        assert ['valgrind', '--error-exitcode=255', '--leak-check=full', '--errors-for-leak-kinds=definite',
                 '--suppressions=file'] == cmd_args
 
     def test_generate_command_logfile(self):
         default_valgrind = Valgrind(options="")
         cmd_args = default_valgrind.generate_command('logfile')
-        assert ['valgrind', '--error-exitcode=1', '--leak-check=full', '--errors-for-leak-kinds=definite',
+        assert ['valgrind', '--error-exitcode=255', '--leak-check=full', '--errors-for-leak-kinds=definite',
                 '--log-file=logfile'] == cmd_args
