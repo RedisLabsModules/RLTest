3637fe68359076278590c1a7c7cd8a091037d9a1
alon <alonreshef24@gmail.com>
2023-12-21 14:30:52 +0200

    support adding shard to cluster

--- RLTest/env.py
+++ RLTest/env.py
@@ -389,6 +389,13 @@ class Env:
         else:
             return self.getConnection()
 
+    def addShardToClusterIfExists(self):
+        if isinstance(self.envRunner, ClusterEnv):
+            test_fname = self.testName.replace(':', '_')
+            output_files_format = '%s-' + '%s-oss-cluster' % test_fname
+            kwargs = self.getEnvKwargs()
+            return self.envRunner.addShardToCluser(self.redisBinaryPath, output_files_format, **kwargs)
+
     def getSlaveConnection(self):
         return self.envRunner.getSlaveConnection()
 
