540bce38bf7f5c0c8f74f2b3ea7638359b5ece2b
Chayim I. Kirshen <c@kirshen.com>
2024-02-29 10:55:21 +0200

    fixing pkg_resources, not present

--- RLTest/_version.py
+++ RLTest/_version.py
@@ -1,8 +1,8 @@
 
 # This attribute is the only one place that the version number is written down,
 # so there is only one place to change it when the version number changes.
-import pkg_resources
 try:
+    import pkg_resources
     __version__ = pkg_resources.get_distribution('RLTest').version
-except (pkg_resources.DistributionNotFound, AttributeError):
+except (pkg_resources.DistributionNotFound, AttributeError, ImportError):
     __version__ = "99.99.99"  # like redis modules
