ff390dc99df84c58a1939026dc727cd767bb5b3f
nafraf <nafraf@gmail.com>
2024-11-18 16:48:48 -0500

    Validate redis config file is not used with enterprise cluster

--- RLTest/__main__.py
+++ RLTest/__main__.py
@@ -546,6 +546,9 @@ class RLTest:
         if Defaults.use_unix and Defaults.use_slaves:
             raise Exception('Cannot use unix sockets with slaves')
 
+        if Defaults.env == 'enterprise-cluster' and Defaults.redis_config_file is not None:
+            raise Exception('Redis configuration file is not supported with enterprise-cluster env')
+
         self.tests = []
         self.testsFailed = {}
         self.currEnv = None
