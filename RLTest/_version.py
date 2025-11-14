c303ce83b700aa4c9fc7d06172a4e7173d434449
GuyAv46 <guy.avimor@gmail.com>
2025-11-14 17:38:31 +0200

    remove usage of pkg_resources

--- RLTest/_version.py
+++ RLTest/_version.py
@@ -2,7 +2,7 @@
 # This attribute is the only one place that the version number is written down,
 # so there is only one place to change it when the version number changes.
 try:
-    import pkg_resources
-    __version__ = pkg_resources.get_distribution('RLTest').version
-except (pkg_resources.DistributionNotFound, AttributeError, ImportError):
+    from importlib.metadata import version
+    __version__ = version('RLTest')
+except Exception:
     __version__ = "99.99.99"  # like redis modules
