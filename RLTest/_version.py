603aac7341a715b5a2b6a4cccd41fd66ac0f9579
GuyAv46 <guy.avimor@gmail.com>
2025-11-15 12:03:43 +0200

    support version on python 3.7

--- RLTest/_version.py
+++ RLTest/_version.py
@@ -3,6 +3,13 @@
 # so there is only one place to change it when the version number changes.
 try:
     from importlib.metadata import version
+except ImportError:
+    try: # For Python<3.8
+        from importlib_metadata import version # type: ignore
+    except ImportError:
+        version = None
+
+try:
     __version__ = version('RLTest')
 except Exception:
     __version__ = "99.99.99"  # like redis modules
